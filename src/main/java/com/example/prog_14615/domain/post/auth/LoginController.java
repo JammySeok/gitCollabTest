b04981713adff386a30a8f2fb104a16fe5101385
JammySeok <jjh326326@naver.com>
2025-11-29 02:49:25 +0900

    로그인 페이지
    (Rest API) 로그인페이지 작동되게 구현
    JWT x

---
+++ src/main/java/com/example/prog_14615/domain/post/auth/LoginController.java
@@ -0,0 +1,62 @@
+package com.example.prog_14615.domain.post.auth;
+
+import com.example.prog_14615.domain.post.member.Member;
+import com.example.prog_14615.domain.post.member.MemberRepository;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Controller
+public class LoginController {
+
+    private final MemberRepository memberRepository;
+
+    @Getter
+    @NoArgsConstructor
+    static class LoginReqBody {
+        String username;
+        String password;
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "redirect:/login.html";
+    }
+
+    @PostMapping("/login")
+    @ResponseBody
+    public Map<String, Object> login(@RequestBody LoginReqBody loginReqBody) {
+
+        Optional<Member> opMember = memberRepository.findByUsername(loginReqBody.username);
+
+        Map<String, Object> response = new HashMap<>();
+
+        if(opMember.isEmpty()) {
+            response.put("resultCode", "401");
+            response.put("msg", "회원을 찾을 수 없습니다.");
+            return response;
+        }
+
+        Member member = opMember.get();
+        if(!member.getPassword().equals(loginReqBody.password)) {
+            response.put("resultCode", "401");
+            response.put("msg", "아이디 혹은 비밀번호가 다릅니다.");
+            return response;
+        }
+
+        response.put("resultCode", "200");
+        response.put("data", member.getUsername());
+
+        return response;
+    }
+}
